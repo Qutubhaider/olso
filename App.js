6105858f89b019ca148df8bd5e4f883fab273a98
Md Qutubuddin Haider <35701735+Qutubhaider@users.noreply.github.com>
2020-01-18 11:40:45 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import Home from "./components/Home"
+import UserProfile from "./components/user-profile"
+import HelpSupport from "./components/Help&Support"
+import Onboarding from "./components/onboarding"
+import AddProduct from "./components/AddProduct"
+import MyProduct from "./components/MyProduct"
+import Stats from "./components/stats"
+import IndexUserInfo from "./components/IndexUserInfo"
+import NavigationDrawer from "./components/NavigationDrawer"
+import Payment from './components/Payment';
 import {
     StyleSheet,
+    ScrollView,
     View
-} from 'react-native';;
+} from 'react-native';
 
 export default class App extends Component {
     render() {
         return (
         <View style={styles.body}>
-              <View style={styles.wrapper}>
-                    <Home />
-              </View>
+            {/*<View style={styles.wrapper}>
+                  <ScrollView>
+                 <Home />
+                  </ScrollView>
+        </View>*/}
+             <ScrollView>
+             {/*   <UserProfile />*/}
+                 {/*<Stats/>*/}
+                 {/*<IndexUserInfo/>*/}
+             </ScrollView>
+             {/*<MyProduct/>*/}
+            {/*<NavigationDrawer/>*/}
+            <Payment/>
+                {/* <HelpSupport />*/}
+           {/*<Onboarding />*/}
+           {/*<AddProduct /> --need to take a look*/ }       
         </View>
         );
     }
@@ -20,7 +43,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     body:{
-        backgroundColor:"#fafafa",
+        backgroundColor:"#EBEDEF",
         flex:1,
         justifyContent: "center",
         alignItems:"center"
